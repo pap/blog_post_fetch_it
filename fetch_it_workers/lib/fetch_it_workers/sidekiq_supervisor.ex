e841221cc46fee3877482e907929eeab03b02f28
Paulo A Pereira <odesassossegado@gmail.com>
2015-11-03 11:23:54 +0000

    wip

--- fetch_it_workers/lib/fetch_it_workers/sidekiq_supervisor.ex
+++ fetch_it_workers/lib/fetch_it_workers/sidekiq_supervisor.ex
@@ -1,6 +1,8 @@
 defmodule FetchItWorkers.SidekiqSupervisor do
   use Supervisor
 
+  # TODO: add poolboy !
+
   def start_link(redis_client) do
     Supervisor.start_link(__MODULE__, [redis_client], name: __MODULE__)
   end
